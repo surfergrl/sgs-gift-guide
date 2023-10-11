a018601e2c2e74e9486f0274e430b05d2c2664b8
surfergrl <>
2023-10-11 12:38:31 +0000

    correcting form logic

--- script.js
+++ script.js
@@ -1,51 +1,49 @@
 //Change intro div to first question
-    //     let content = document.getElementById('contentBox');
-    //     let html = `
-    //      <h4>First Question</h4>
-    //         <p>What category of jewellery would you like to see?</p>
-    //         <ul>
-    //             <li>Bracelets</li>
-    //             <li>Ring</li>
-    //             <li>Anklets</li>
-    //             <li>Keyrings</li>
-    //             <li>Necklaces</li>
-    //             <li>Earrings</li>
-    //         </ul>
-    //     <button onclick="start()">Next</button>    
-    //     <button onclick="start()">Restart</button>
-    //     `;
-    //     content.innerHTML = html;
-    // }
-    //Change first question div to second question 
+//     let content = document.getElementById('contentBox');
+//     let html = `
+//      <h4>First Question</h4>
+//         <p>What category of jewellery would you like to see?</p>
+//         <ul>
+//             <li>Bracelets</li>
+//             <li>Ring</li>
+//             <li>Anklets</li>
+//             <li>Keyrings</li>
+//             <li>Necklaces</li>
+//             <li>Earrings</li>
+//         </ul>
+//     <button onclick="start()">Next</button>    
+//     <button onclick="start()">Restart</button>
+//     `;
+//     content.innerHTML = html;
+// }
+//Change first question div to second question 
 
 //form logic 
 function firstQuestion() {
     form.addEventListener("submit", (event) => {
         event.preventDefault();
-    } 
-
-  let formData = {
-        category: form.querySelector(".question1.select").value,
-        gender: form.querySelector(".question2.select").value,
-        design: form.querySelector(".question3.select").value,
-        quality: form.querySelector(".question4.select").value,
-        occasion: form.querySelector(".question5.select").value,
-    };
-
-    filterIems(formData);
-});
+        let formData = {
+            category: form.querySelector(".category.select").value,
+            gender: form.querySelector(".budget.select").value,
+            design: form.querySelector(".theme.select").value,
+            quality: form.querySelector(".style.select").value,
+            occasion: form.querySelector(".gender.select").value,
+        };
+        filterItems(formData);
+    });
+};
 
 function filterItems(formData) {
-    const filtered = jewelleryItems.filter("jewellery" Llangrannog 
-        (v) => 
-        v.category === FormData.category && 
+    const filtered = jewelleryItems.filter("jewellery" Llangrannog
+        (v) =>
+    v.category === FormData.category &&
         v.gender === formData.gender &&
         v.budget === formData.budget &&
         v.occasion === formData.occasion &&
         v.quality === formData.quality 
-        ); 
+        );
     console.log(filtered);
-} 
+}
 
 // Define an array of items with their properties
 var jewelleryItems = [
@@ -53,57 +51,57 @@ var jewelleryItems = [
         name: "Carreg Bica pendant",
         category: "Pendants",
         price: 35,
-        hers: "Unisex",
-        budget: "med",
+        gender: "Unisex",
+        budget: "high",
         style: "Llangrannog",
     },
     {
         name: "Anchor and seaglass pendant",
         category: "Pendants",
         price: 25,
-        hers: "Unisex",
+        gender: "Unisex",
         budget: "med",
         style: "Classic",
     },
     {
-        name: "Carreg Bica pendant",
-        category: "Pendants",
+        name: "Silver Cariad (Love) Ring",
+        category: "Rings",
         price: 35,
-        hers: "Unisex",
-        budget: "med",
-        style: "Llangrannog",
+        gender: "Unisex",
+        budget: "high",
+        style: "Cariad",
     },
     {
-        name: "Carreg Bica pendant",
-        category: "Pendants",
-        price: 35,
-        hers: "Unisex",
+        name: "Seaglass Charm Anklet",
+        category: "Anklets",
+        price: 20,
+        gender: "Unisex",
         budget: "med",
-        style: "Llangrannog",
+        style: "Delicate",
     },
     {
-        name: "Carreg Bica pendant",
-        category: "Pendants",
-        price: 35,
-        hers: "Unisex",
+        name: "Charm Bracelet",
+        category: "Bracelets",
+        price: 15,
+        gender: "Hers",
         budget: "med",
-        style: "Llangrannog",
+        style: "Chunky",
     },
     {
-        name: "Carreg Bica pendant",
-        category: "Pendants",
-        price: 35,
-        hers: "Unisex",
-        budget: "med",
-        style: "Llangrannog",
+        name: "Shell & Sand Keyring",
+        category: "Keyrings",
+        price: 6.50,
+        gender: "Unisex",
+        budget: "low",
+        style: "Quirky",
     },
 
 ];
 
 // Function to ask a question and return the user's answer
-function askQuestion(question) {
-    return alert(question);
-}
+// function askQuestion(question) {
+//     return alert(question);
+// }
 
 // Function to filter items based on user preferences
 function filterjewelleryItems(preferences) {
@@ -136,16 +134,16 @@ function displayRecommendedItems(items) {
     }
 }
 
-// Ask questions to determine user preferences
-var preferences = {};
-preferences.category = askQuestion("What category of jewellery do you want to see?");
-preferences.hers = askQuestion("His, hers or both?");
-preferences.style = askQuestion("What style do you prefer?");
-preferences.budget = askQuestion("What is your budget?");
-preferences.theme = askQuestion("Choose a theme");
+// // Ask questions to determine user preferences
+// var preferences = {};
+// preferences.category = askQuestion("What category of jewellery do you want to see?");
+// preferences.hers = askQuestion("His, hers or both?");
+// preferences.style = askQuestion("What style do you prefer?");
+// preferences.budget = askQuestion("What is your budget?");
+// preferences.theme = askQuestion("Choose a theme");
 
-// Filter items based on user preferences
-var recommendedItems = filterjewelleryItems(preferences);
+// // Filter items based on user preferences
+// var recommendedItems = filterjewelleryItems(preferences);
 
-// Display recommended items
-displayRecommendedItems(recommendedItems);
+// // Display recommended items
+// displayRecommendedItems(recommendedItems);
