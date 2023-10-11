7c1b9876d5e0a5e0ac8cbb5cdb498fb13ec9a57e
surfergrl <>
2023-10-11 13:12:22 +0000

    js for results display

--- script.js
+++ script.js
@@ -1,51 +1,4 @@
-//Change intro div to first question
-//     let content = document.getElementById('contentBox');
-//     let html = `
-//      <h4>First Question</h4>
-//         <p>What category of jewellery would you like to see?</p>
-//         <ul>
-//             <li>Bracelets</li>
-//             <li>Ring</li>
-//             <li>Anklets</li>
-//             <li>Keyrings</li>
-//             <li>Necklaces</li>
-//             <li>Earrings</li>
-//         </ul>
-//     <button onclick="start()">Next</button>    
-//     <button onclick="start()">Restart</button>
-//     `;
-//     content.innerHTML = html;
-// }
-//Change first question div to second question 
-
-//form logic 
-function firstQuestion() {
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        let formData = {
-            category: form.querySelector(".category.select").value,
-            gender: form.querySelector(".budget.select").value,
-            design: form.querySelector(".theme.select").value,
-            quality: form.querySelector(".style.select").value,
-            occasion: form.querySelector(".gender.select").value,
-        };
-        filterItems(formData);
-    });
-};
-
-function filterItems(formData) {
-    const filtered = jewelleryItems.filter("jewellery" Llangrannog
-        (v) =>
-    v.category === FormData.category &&
-        v.gender === formData.gender &&
-        v.budget === formData.budget &&
-        v.occasion === formData.occasion &&
-        v.quality === formData.quality 
-        );
-    console.log(filtered);
-}
-
-// Define an array of items with their properties
+// Define an array of jewellery items with their properties
 var jewelleryItems = [
     {
         name: "Carreg Bica pendant",
@@ -98,52 +51,77 @@ var jewelleryItems = [
 
 ];
 
-// Function to ask a question and return the user's answer
-// function askQuestion(question) {
-//     return alert(question);
+//form logic 
+function firstQuestion() {
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        let formData = {
+            category: form.querySelector(".category.select").value,
+            gender: form.querySelector(".budget.select").value,
+            design: form.querySelector(".theme.select").value,
+            quality: form.querySelector(".style.select").value,
+            occasion: form.querySelector(".gender.select").value,
+        };
+        filterItems(formData);
+    });
+};
+
+function filterItems(formData) {
+    const filtered = jewelleryItems.filter((v) =>
+        v.category === formData.category &&
+        v.gender === formData.gender &&
+        v.budget === formData.budget &&
+        v.occasion === formData.occasion &&
+        v.quality === formData.quality
+    );
+    console.log(filtered);
+}
+
+// // Function to display recommended jewelry items
+// function displayRecommendedItems(items) {
+//     if (items.length === 0) {
+//         console.log("Sorry, we don't have any items matching your preferences.");
+//     } else {
+//         console.log("Here are some ideas:");
+//         for (var i = 0; i < items.length; i++) {
+//             console.log(items[i].name + " - £" + items[i].price);
+//         }
+//     }
 // }
+    // Get the results element in index.html 
+    const resultsElement = document.getElementById("results");
+
+    // Clear any previous content (just in case)
+    resultsElement.innerHTML = "";
+
+    // Create an unordered list to display the results
+    const ul = document.createElement("ul");
+
+    // Iterate through the filtered items and create list items
+    filtered.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = item.name; // customise this later to display imgs
+        ul.appendChild(li);
+    });
+
+    // Append the list to the results element
+    resultsElement.appendChild(ul);
 
 // Function to filter items based on user preferences
-function filterjewelleryItems(preferences) {
-    var filteredItems = [];
-    for (var i = 0; i < jewelleryItems.length; i++) {
-        var item = jewelleryItems[i];
-        var match = true;
-        for (var key in preferences) {
-            if (preferences.hasOwnProperty(key) && item[key] !== preferences[key]) {
-                match = false;
-                break;
-            }
-        }
-        if (match) {
-            filteredItems.push(item);
-        }
-    }
-    return filteredItems;
-}
-
-// Function to display recommended jewelry items
-function displayRecommendedItems(items) {
-    if (items.length === 0) {
-        console.log("Sorry, we don't have any items matching your preferences.");
-    } else {
-        console.log("Here are some ideas:");
-        for (var i = 0; i < items.length; i++) {
-            console.log(items[i].name + " - $" + items[i].price);
-        }
-    }
-}
-
-// // Ask questions to determine user preferences
-// var preferences = {};
-// preferences.category = askQuestion("What category of jewellery do you want to see?");
-// preferences.hers = askQuestion("His, hers or both?");
-// preferences.style = askQuestion("What style do you prefer?");
-// preferences.budget = askQuestion("What is your budget?");
-// preferences.theme = askQuestion("Choose a theme");
-
-// // Filter items based on user preferences
-// var recommendedItems = filterjewelleryItems(preferences);
-
-// // Display recommended items
-// displayRecommendedItems(recommendedItems);
+// function filterjewelleryItems(preferences) {
+//     var filteredItems = [];
+//     for (var i = 0; i < jewelleryItems.length; i++) {
+//         var item = jewelleryItems[i];
+//         var match = true;
+//         for (var key in preferences) {
+//             if (preferences.hasOwnProperty(key) && item[key] !== preferences[key]) {
+//                 match = false;
+//                 break;
+//             }
+//         }
+//         if (match) {
+//             filteredItems.push(item);
+//         }
+//     }
+//     return filteredItems;
+// }
