f1968b29df818509f535fa4e36a1fdb65aee0de7
Kat Dawes <kldawes@gmail.com>
2025-02-02 12:39:57 +0000

    Add code to display questions on page

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 
-// Create an array to store the questions in objects
+// Create an array to store the questions in objects along with their weighting 
 const quizQuestions = [
-    {
-        question: "What type of jewellery does this person wear?",
-        options: {
-            a: "Bracelets",
-            b: "Rings",
-            c: "Keyrings",
-            d: "Necklaces"
-            e: "Earrings"
-        },
-        correctAnswer: "a"
-    }
+        {
+          question: "What type of jewellery does this person wear?",
+          options: {
+            a: { text: "Bracelets", weight: 1 },
+            b: { text: "Rings", weight: 2 },
+            c: { text: "Keyrings", weight: 3 },
+            d: { text: "Necklaces", weight: 4 },
+            e: { text: "Earrings", weight: 5 }
+          }
+        }
 
     {
         question: "What price range are you interested in?",
@@ -59,4 +58,38 @@ const quizQuestions = [
         correctAnswer: "a"
     }
 ]
+
+// Code to show each quiz question on the page 
+function buildQuiz(){
+    // Store the output in here:
+    const output = [];
+
+    // For each question (loop over)... 
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+
+            //Variable to store the list of poss options 
+            const options = [];
+
+            //For each option, denoted by letter a b c d or e 
+            for(letter in currentQuestion.options) {
+            //Add an html radio button 
+            `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.options[letter]}
+              </label>`
+            }
+
+            // Add this question and its answers to the output
+            output.push(
+            `<div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${options.join('')} </div>`
+            );
+        }
+        // Combine  output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join('');
+    
+    );
+}
 
